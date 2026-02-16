3dbf8b9008d1e1cdfaf3f02d9ff23074ff78de6b
sonuthakur03 <rsonuth789@gmail.com>
2026-02-16 22:33:30 +0545

    feat: update Jokes component to accept props and render multiple jokes

--- 03jokeProps/src/App.jsx
+++ 03jokeProps/src/App.jsx
@@ -17,6 +17,16 @@
  * show the punchline?
  */
 
+/**
+ * Challenge: Think critically - how would you pass in a prop that
+ * isn't a string datatype.
+ *
+ * E.g. Say you want each Joke component to receive an "upvotes"
+ * and "downvotes"prop that is a number, as well as a prop with
+ * an array of comments, and a boolean of whether the joke is a
+ * pun (`isPun`).
+ */
+
 import Jokes from "./components/Jokes";
 
 function App() {
@@ -26,7 +36,21 @@ function App() {
         id="jokes"
         className="h-screen flex items-center justify-center bg-gray-800"
       >
-        <Jokes />
+        <div className="flex flex-col mx-auto  justify-center p-3 bg-amber-200 w-[50%] rounded-md gap-3">
+          <h2 className="text-2xl font-semibold">
+            Jokes that will make you laugh till your death.
+          </h2>
+          <hr />
+          <Jokes
+            id="1"
+            setup="I got my daughter a fridge for her birthday."
+            punchline="I can't wait to see her face light up when she opens it."
+          />
+          <Jokes
+            id="2"
+            punchline="It's hard to explain puns to kleptomaniacs because they always take things literally."
+          />
+        </div>
       </section>
     </>
   );
