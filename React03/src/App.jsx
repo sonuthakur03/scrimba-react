76d139c418d760e9565659d2460a4d963d6612d6
sonuthakur03 <rsonuth789@gmail.com>
2026-02-18 11:18:18 +0545

    feat: pass props to Entry component for dynamic data rendering

--- React03/src/App.jsx
+++ React03/src/App.jsx
@@ -1,3 +1,14 @@
+/**
+ * Challenge: pass props to the Entry component to display
+ * its data. See the `data.md` file for each prop name and its
+ * value.
+ *
+ * Then on the Entry component, receive and display the values
+ * for those props. In the end, the page should look the same
+ * as it does now, but without all the hard-coded data in the
+ * component
+ */
+
 import Entry from "./components/Entry";
 import Header from "./components/Header";
 
@@ -9,7 +20,17 @@ function App() {
         className="flex justify-center items-center flex-col gap-8 border border-black rounded-sm"
       >
         <Header />
-        <Entry />
+        <Entry
+          img={{
+            src: "https://imgs.search.brave.com/3_ZiPQqSO-7UyE23KaxEG-edQZJX-DN4QQiqwHnsW70/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAyMi8w/My8yMC8xNS80MC9u/YXR1cmUtNzA4MTEz/OF82NDAuanBn",
+            alt: "Mount Fuji",
+          }}
+          title="Mount Fuji"
+          country="Japan"
+          googleMapsLink="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
+          dates="12 Jan, 2021 - 24 Jan, 2021"
+          text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+        />
       </div>
     </div>
   );
--- React03/src/components/Entry.jsx
+++ React03/src/components/Entry.jsx
@@ -15,44 +15,31 @@
  */
 import { FaMapMarkerAlt } from "react-icons/fa";
 
-const Entry = () => {
+const Entry = ({ img, title, country, googleMapsLink, dates, text }) => {
   return (
     <div
       className="flex w-117.5 h-52 justify-center items-center gap-6"
       style={{ fontFamily: "Inter , sans-serif" }}
     >
       <div id="image" className="w-30.25 h-42 rounded-sm shrink-0">
-        <img
-          src="https://imgs.search.brave.com/3_ZiPQqSO-7UyE23KaxEG-edQZJX-DN4QQiqwHnsW70/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAyMi8w/My8yMC8xNS80MC9u/YXR1cmUtNzA4MTEz/OF82NDAuanBn"
-          alt="mt fuji"
-          className="h-42 w-full rounded-sm "
-        />
+        <img src={img.src} alt={img.alt} className="h-42 w-full rounded-sm " />
       </div>
       <div id="info" className="flex flex-col justify-center">
         <div id="info-header" className="flex items-center gap-2">
           <FaMapMarkerAlt className="text-[#f55a5a] h-3.5 w-2.5" />
           <p className="text-[#2B283A] font-light tracking-[4px] text-[10px] ">
-            JAPAN
+            {country}
           </p>
           <a
-            href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu
-"
+            href={googleMapsLink}
             className="text-[#918E9B] underline text-[10px]"
           >
             View on Google Maps
           </a>
         </div>
-        <h2 className="text-[#2B283A] font-extrabold text-3xl mb-3">
-          Mount Fuji
-        </h2>
-        <p className="text-[12px] font-extrabold my-1.5">
-          12 Jan, 2023 - 24 Jan, 2023
-        </p>
-        <p className="text-[10px]">
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
-        </p>
+        <h2 className="text-[#2B283A] font-extrabold text-3xl mb-3">{title}</h2>
+        <p className="text-[12px] font-extrabold my-1.5">{dates}</p>
+        <p className="text-[10px]">{text}</p>
       </div>
     </div>
   );
