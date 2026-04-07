569672b5bd28da935fb8cd249c9e52fe468f3998
sonuthakur03 <rsonuth789@gmail.com>
2026-04-07 23:29:38 +0545

    feat: add Header component and integrate it into App

---
+++ 8MemeGenerator/src/components/Header.jsx
@@ -0,0 +1,20 @@
+const Header = () => {
+  return (
+    <>
+      <header
+        className="flex items-center gap-4 p-4 text-white"
+        style={{
+          background: "linear-gradient(90deg, #672280 1.18%, #A626D3 100%)",
+        }}
+      >
+        <img
+          src="https://media.tenor.com/AXY-D-1UU-MAAAAm/troll-lol.webp"
+          alt=""
+        />
+        <h1>Meme Generator</h1>
+      </header>
+    </>
+  );
+};
+
+export default Header;
