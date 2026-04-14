bbd9b445074e1647056291ec5b06c4b22d45729e
sonuthakur03 <rsonuth789@gmail.com>
2026-04-14 20:39:04 +0545

    feat: implement Die component and render multiple instances in App

--- 11Tenzies/src/App.jsx
+++ 11Tenzies/src/App.jsx
@@ -1,8 +1,21 @@
+import { Die } from "./components/Die";
+
 function App() {
   return (
     <main className="bg-[#0B2434] h-screen flex flex-col items-center justify-center p-4">
       <p className="bg-[#F5F5F5] rounded-lg max-w-[400px] max-h-[400px] w-full h-screen">
-        somethinhs heere
+        <div>
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+        </div>
       </p>
     </main>
   );
